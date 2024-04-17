e774268228e04b9a4cf420255bc96e0295924a67
James Ashok <jamesashok93@gmail.com>
2024-04-17 09:21:42 +0530

    Post title and hero image as pattern.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # whale-word
 A practice wordpress theme
+- based on tutorial from [WordPress Block Theme Development | Elliott Richmond | YouTube](https://www.youtube.com/watch?v=uR2Q2Pn_zSU)
---
+++ wp-content/themes/whaleword/patterns/title-hero.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Title Hero
+ * Slug: whaleword/title-hero
+ * Categories: features
+ */
+?>
+
+<!-- wp:post-title {"isLink":true} /-->
+<!-- wp:post-featured-image {"isLink":true} /-->
